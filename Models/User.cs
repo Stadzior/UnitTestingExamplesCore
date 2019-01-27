1489453ec310aac5941b501f9507d4aee237cd36
Stadzior <Stadzior@gmail.com>
2019-01-27 14:16:20 +0100

    Wicked design.

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using app.Services;
+
+namespace app.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public double Balance { get; set; }
+        public bool IsVip { get; set; }
+
+        private ShoppingCart shoppingCart;
+
+        public User()
+        {
+            shoppingCart = new ShoppingCart();
+        }
+
+        public void Checkout()
+        {
+            shoppingCart.Checkout(this);
+        }
+    }
+}
