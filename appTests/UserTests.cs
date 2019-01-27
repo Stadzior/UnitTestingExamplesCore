6d573b9981a5d10d0ecd03ef3d343b38466d80e8
Stadzior <Stadzior@gmail.com>
2019-01-27 23:17:12 +0100

    Revert "Removed files from appTests."
    This reverts commit 3cf6c76155b2d580265264969e389075d474708d.

---
+++ appTests/UserTests.cs
@@ -0,0 +1,15 @@
+﻿using app.Models;
+using NUnit.Framework;
+
+namespace appTests
+{
+    [TestFixture]
+    public class UserTests
+    {
+        [TestCase]
+        public void Given_AverageUser_()
+        {
+            var user = new User { IsVip = true };
+        }
+    }
+}
