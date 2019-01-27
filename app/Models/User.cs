c0e3139fc82b737ae99fa6c1946d7958bc6a5afe
Stadzior <Stadzior@gmail.com>
2019-01-27 23:17:16 +0100

    Revert "Removed test project from master."
    This reverts commit 17307f29de974381ade36fc1a6d936dff96d2b1d.

---
+++ app/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using app.Services;
+using System;
+
+namespace app.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public double Balance { get; set; }
+        public bool IsVip { get; set; }
+
+        private ShoppingCart shoppingCart;
+
+        public User()
+        {
+            shoppingCart = new ShoppingCart();
+        }
+
+        public void Checkout()
+        {
+            if (IsVip)
+                Console.WriteLine("You're V.I.P");
+            shoppingCart.Checkout(this);
+        }
+    }
+}
